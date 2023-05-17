16447074ca7716defbf7fddc8e5d5300886cc0c3
legendarykamal <geniusamansingh@gmail.com>
2023-05-17 20:27:14 +0530

    chakra next crud application

---
+++ pages/blog-posts/edit/[id].tsx
@@ -0,0 +1,49 @@
+import { ChakraUIEditInferencer } from "@refinedev/inferencer/chakra-ui";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+
+export default function BlogPostEdit() {
+  return (
+    <ChakraUIEditInferencer
+      fieldTransformer={(field: any) => {
+        if (["$permissions", "$updatedAt"].includes(field.key)) {
+          return false;
+        }
+
+        if (field.key === "$createdAt") {
+          field.key = "created_at";
+          field.title = "Created At";
+        }
+
+        return field;
+      }}
+    />
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
+    return {
+      props: {
+        ...translateProps,
+      },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/blog-posts")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
---
+++ src/authProvider.ts
@@ -0,0 +1,100 @@
+import { AppwriteException } from "@refinedev/appwrite";
+import { AuthBindings } from "@refinedev/core";
+import nookies from "nookies";
+
+import { account, appwriteClient, APPWRITE_TOKEN_KEY } from "./utility";
+
+export const authProvider: AuthBindings = {
+  login: async ({ email, password }) => {
+    try {
+      await account.createEmailSession(email, password);
+      const { jwt } = await account.createJWT();
+
+      if (jwt) {
+        nookies.set(null, APPWRITE_TOKEN_KEY, jwt, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: "/",
+        });
+      }
+
+      return {
+        success: true,
+        redirectTo: "/",
+      };
+    } catch (error) {
+      const { type, message, code } = error as AppwriteException;
+      return {
+        success: false,
+        error: {
+          message,
+          name: `${code} - ${type}`,
+        },
+      };
+    }
+  },
+  logout: async () => {
+    try {
+      await account.deleteSession("current");
+    } catch (error: any) {
+      return {
+        success: false,
+        error,
+      };
+    }
+    nookies.destroy(null, APPWRITE_TOKEN_KEY);
+    appwriteClient.setJWT("");
+    return {
+      success: true,
+      redirectTo: "/login",
+    };
+  },
+  onError: async (error) => {
+    console.error(error);
+    return { error };
+  },
+  check: async (ctx) => {
+    // for server side authentication
+    const cookies = nookies.get(ctx);
+    const appwriteJWT = cookies[APPWRITE_TOKEN_KEY];
+    if (appwriteJWT) {
+      appwriteClient.setJWT(appwriteJWT);
+    }
+
+    try {
+      const session = await account.get();
+
+      if (session) {
+        return {
+          authenticated: true,
+        };
+      }
+    } catch (error: any) {
+      return {
+        authenticated: false,
+        error: error,
+        logout: true,
+        redirectTo: "/login",
+      };
+    }
+
+    return {
+      authenticated: false,
+      error: {
+        message: "Check failed",
+        name: "Session not found",
+      },
+      logout: true,
+      redirectTo: "/login",
+    };
+  },
+  getPermissions: async () => null,
+  getIdentity: async () => {
+    const user = await account.get();
+
+    if (user) {
+      return user;
+    }
+
+    return null;
+  },
+};
